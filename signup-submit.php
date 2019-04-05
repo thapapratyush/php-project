6c3b4d64419dd826a3bf94eefe48093946a5c64b
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-04 23:25:24 -0400

    Added database connection after validation

--- signup-submit.php
+++ signup-submit.php
@@ -1,24 +1,28 @@
 <?php include("top.html"); ?>
 <?php
+    $logs = "";
     function validate_data(){
          if($_SERVER["REQUEST_METHOD"] == "POST"){
             if(!is_numeric($_POST["age"])) {
-                echo "ERROR! Age should be a number.";
+                $logs += "ERROR! Age should be a number. \n";
             }
 
             if (!in_array($_POST["personality_type"], [STJ, ISFJ,INFJ, INTJ, ISFP, INFP, INTP, ESTP, ESFP, ENFP, ENTP, ESTJ, ESFJ, ENFJ, ENTJ], true)) {
-                echo "Error! Enter a Keirsey personality type!";
+                $logs += "Error! Enter a Keirsey personality type! \n";
             }
 
             if (!is_numeric($_POST["max_seek_age"] || !is_numeric($_POST["min_seek_age"]))) { 
-                echo "Error! The seeking age has to be a number.";
+                $logs += "Error! The seeking age has to be a number. \n";
             }
 
             //implement name check
             //implement writing to server or file
         }
-
-         
+    }
+    validate_data();
+    if ($logs !== ""){          //if no errors connect to database
+        $db = new PDO("mysql:dbname=singles;host=localhost", "root", "toor");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 ?>
 
