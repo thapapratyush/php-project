310c9c7eb636a0bd2236b8603924ec46f34e6e54
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-04 21:04:17 -0400

    Add validation to signup-submit

--- signup-submit.php
+++ signup-submit.php
@@ -1,3 +1,25 @@
 <?php include("top.html"); ?>
+<?php
+    function validate_data(){
+         if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if(!is_numeric($_POST["age"])) {
+                echo "ERROR! Age should be a number.";
+            }
+
+            if (!in_array($_POST["personality_type"], [STJ, ISFJ,INFJ, INTJ, ISFP, INFP, INTP, ESTP, ESFP, ENFP, ENTP, ESTJ, ESFJ, ENFJ, ENTJ], true)) {
+                echo "Error! Enter a Keirsey personality type!";
+            }
+
+            if (!is_numeric($_POST["max_seek_age"] || !is_numeric($_POST["min_seek_age"]))) { 
+                echo "Error! The seeking age has to be a number.";
+            }
+
+            //implement name check
+            //implement writing to server or file
+        }
+
+         
+    }
+?>
 
 <?php include("bottom.html"); ?>
