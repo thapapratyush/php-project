2f55c7ff6e34076b39b0a991b94b690b5008dba7
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-04 23:12:14 -0400

    Add matches.php

---
+++ matches.php
@@ -0,0 +1,11 @@
+<?php include("top.html"); ?>
+
+<form action="matches-submit.php" method="get">
+        <div> Returning User: </div>
+        <strong class="column"> Name: </strong>
+        <input type="text" name="name" maxlength="16"><br><br>
+        <input type="submit" value="View My Matches">
+    </form>
+</div>
+
+<?php include("top.html"); ?>
